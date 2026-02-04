7fbe0a9a32008a3b9b56bd974b51d60313ebee9e
kcrachapudi <kcrachapudi@gmail.com>
2026-02-03 22:49:56 -0600

    first commit

--- deploy_favorites.py
+++ deploy_favorites.py
@@ -2,6 +2,8 @@ from vyper import compile_code
 
 def main():
     print("Let's read in the Vyper code and deploy it!")
+    with open("Favorites.vy", "r") as fav_file:
+        favorites_code = fav_file.read()
     compilation_details = compile_code(favorites_code, output_formats["bytecode"])
     print("Compilation details:", compilation_details)
     
